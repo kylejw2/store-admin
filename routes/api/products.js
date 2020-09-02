c2001148bb89efc838ab7c375841a46fcdc9b76e
Kyle Wright <kylejamwright@gmail.com>
2020-09-02 11:01:40 -0600

    Implemented Read products in the service layer

---
+++ routes/api/products.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+// The create product function is in the images.js file of this folder
+const {
+  readProducts
+} = require('../../data/products');
+
+// GET the products
+router.get('/', async (req, res) => {
+    const response = readProducts();
+    res.send(response);
+})
+
+module.exports = router;
